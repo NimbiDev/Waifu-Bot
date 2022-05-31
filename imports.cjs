c3df8a8f4b542f94a9d33c9bd34c1f7f61fbd6c8
Joshua Lewis <JoshuaVL1988@outlook.com>
2022-05-30 23:24:03 -0700

    :recycle: 05/30/2022 - 11:23 PM

--- imports.cjs
+++ imports.cjs
@@ -1,4 +1,4 @@
-export const client = require('./index.js');
-export const { MessageEmbed, Client, Collection } = require('discord.js');
-export const { inspect } = require("util");
-export const Handler = require('./Handler/handler.js')(client);
+const client = require('./index.js');
+const { MessageEmbed, Client, Collection } = require('discord.js');
+const { inspect } = require("util");
+const Handler = require('./Handler/handler.js')(client);
