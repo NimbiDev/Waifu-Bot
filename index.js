4d0554ef3e4e99fe7d946eac043768fb41aae75a
Joshua Lewis <JoshuaVL1988@outlook.com>
2022-05-30 22:00:27 -0700

    :recycle: 05/30/2022 - 10:00 PM

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-const Cluster = require("discord-hybrid-sharding");
 const { Collection, Client } = require("discord.js");
+require("dotenv").config();
 
 
 const client = new Client({
-    shards: Cluster.data.SHARD_LIST,
-    shardCount: Cluster.data.TOTAL_SHARDS,
     allowedMentions: {
         repliedUser: true,
         parse: ["users", "roles", "everyone"]
     },
-    intents: 131071,
 });
 
-client.cluster = new Cluster.Client(client);
-client.cluster.broadcastEval(c => c.guilds.cache.size)
-    .then(results => console.log(`${results.reduce((prev, val) => prev + val, 0)} total guilds`));
 
 module.exports = client;
 
@@ -23,3 +17,5 @@ client.aliases = new Collection();
 client.SlashCommands = new Collection();
 
 require('./Handler/handler')(client);
+
+client.logon(process.env.TOKEN)
