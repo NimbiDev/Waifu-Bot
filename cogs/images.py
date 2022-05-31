82d9cd29fd67b6d1072eeceb7e47e4499f95507f
Joshua Lewis <JoshuaVL1988@outlook.com>
2022-05-31 03:53:14 -0700

    :recycle: 05/31/2022 - 03:53 AM

--- cogs/images.py
+++ cogs/images.py
@@ -26,13 +26,13 @@ class Images(commands.Cog, name='Image Commands'):
     def cog_unload(self):
         self.client.loop.create_task(self.session.close())
 
-    @commands.command(name='waifu', description='Get a random waifu by tag from waifu.im.\n\n**Example**: {}waifu maid'.format(PREFIX), command_attrs=command_attrs)
+    @commands.command(aliases=['w'], name='waifu', description='Get a random waifu by tag from waifu.im.\n\n**Example**: {}waifu maid'.format(PREFIX), command_attrs=command_attrs)
     @commands.has_guild_permissions(send_messages=True)
     @commands.cooldown(1, 10, commands.BucketType.user)
-    async def _waifu(self, ctx, *, args):
+    async def _waifu(self, ctx, *, tag):
         if ctx.author.id == self.client.user.id:
             return
-        url = 'https://api.waifu.im/random/?{}=true'.format(args)
+        url = 'https://api.waifu.im/random/?{}=true'.format(tag)
         async with self.session.get(url, headers=HEADERS) as resp:
             api = await resp.json()
             if resp.status in {200, 201}:
@@ -46,11 +46,11 @@ class Images(commands.Cog, name='Image Commands'):
                 e.set_description('```js\n{}\n```'.format(error))
                 await ctx.send(embed=e)
     
-    @commands.command(aliases=['gif'], description='Return a random gif by tag from tenor.\nSeperate multiple tags with `+`.\n\n**Example**: `{}tenor anime+hug`'.format(PREFIX), command_attrs=command_attrs)
+    @commands.command(aliases=['t'], name='tenor', description='Return a random gif by tag from tenor.\nSeperate multiple tags with `+`.\n\n**Example**: `{}tenor anime+hug`'.format(PREFIX), command_attrs=command_attrs)
     @commands.has_guild_permissions(send_messages=True, embed_links=True)
     @commands.cooldown(1, 10, commands.BucketType.user)
-    async def tenor(self, ctx, *, query):
-        tenorUrl = await TENOR.arandom(str(query))
+    async def _tenor(self, ctx, *, tag):
+        tenorUrl = await TENOR.arandom(str(tag))
         e = discord.Embed(colour=BLUE)
         e.set_image(url=tenorUrl)
         await ctx.send(embed=e, mention_author=False)
